3d4dd6d8fa47a8598e6736da25694b8d2a806852
kbalodis <krisjanis.balodis@gmail.com>
2013-01-20 13:30:55 +0200

    Added unfinished view template. need to update search function in this template

---
+++ fuel/app/views/template.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title><?php
+if (isset($page_title)) {
+    echo $page_title;
+}
+if (isset($title)) {
+    echo $title;
+}
+?></title>
+	<?php
+	if (isset($libs_js)) {
+	    //some views may want to add extra scripts
+	    echo Asset::js($libs_js);
+	}
+	?>
+
+	<?php echo Asset::css('bootstrap.css'); ?>
+	<?php echo Asset::css('eventual.css'); ?>
+	<?php
+	if (isset($libs_css)) {
+	    //some views may want to add extra stylesheets
+	    echo Asset::css($libs_css);
+	}
+	?>
+    </head>
+    <body>
+	<header>
+	    <h1><a href="/">&laquo;Eventual&raquo; : an event management system</a></h1>
+	    <h3><?php
+                if (isset($page_title)) {
+                    echo $page_title;
+                }
+                if (isset($title)) {
+                    echo $title;
+                }
+            ?></h3>
+
+	    <aside id="auth">	    
+		<?php
+		$auth = Auth::instance();
+		$user_id = $auth->get_user_id();
+		if ($user_id[1] != 0) :
+		    ?>
+		<div id="logged-in">
+		    Logged in as <?php echo $auth->get_email(); ?>
+		</div>
+		<div id="logout">
+			<?php
+			echo Html::anchor("account/logout", "Log out");
+			?>
+    		</div>
+		    <?php
+		else :
+		    echo Html::anchor("account/simpleauth", "Not logged in. Would you like to?");
+		    ?>
+		<?php
+		endif;
+		?>
+	    </aside>
+	</header>
+	<section id="main">
+
+	    <div class="row">
+		<?php if (Session::get_flash('success')): ?>
+    		<div class="alert-message success">
+    		    <p>
+			    <?php echo implode('</p><p>', e((array) Session::get_flash('success'))); ?>
+    		    </p>
+    		</div>
+		<?php endif; ?>
+		<?php if (Session::get_flash('error')): ?>
+    		<div class="alert-message error">
+    		    <p>
+			    <?php echo implode('</p><p>', e((array) Session::get_flash('error'))); ?>
+    		    </p>
+    		</div>
+		<?php endif; ?>
+	    </div>
+
+            <div class="container-fluid">
+                <div class="row-fluid">
+                    <div class="span5">
+                        <?php
+                            if (isset($content)) {
+                                echo $content;
+                            };
+                        ?>
+                    </div>
+                    <div class="span2">
+                        <?php
+                            if (isset($page_content)) {
+                                echo $page_content;
+                            };
+                         ?>
+                    </div>
+                    <div class="span5">
+                        //THIS part is devoted for SEARCH
+                    </div>
+                </div>
+            </div>
+	</section>
+    </body>
+</html>
